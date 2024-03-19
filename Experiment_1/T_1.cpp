ce3563ed69d2049b45fa133acdae968289aac203
Alioth <3434039767@qq.com>
2024-03-19 21:59:21 +0800

    3_19

---
+++ Experiment_1/T_1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+#define MaxSize 5
+
+typedef struct
+{
+    int data[MaxSize];
+    int length = 0;
+} SeqList;
+
+void CreateSeqList(SeqList &L)
+{
+    int i = 0;
+    int temp;
+    cin >> L.data[0];
+    for (i = 0; i < MaxSize; i++)
+    {
+        cin >> temp;
+        for (int j = i; j > -1; j--)
+        {
+
+            if (temp < L.data[j])
+            {
+                L.data[j + 1] = L.data[j];
+                L.data[j] = temp;
+            }
+            else
+                L.data[j + 1] = temp;
+        }
+    }
+}
+void OutputSeqList(SeqList &L)
+{
+
+    for (int i = 0; i < L.length; i++)
+        cout << L.data[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    SeqList L;
+    CreateSeqList(L);
+    OutputSeqList(L);
+    system("pause");
+    return 0;
+}
