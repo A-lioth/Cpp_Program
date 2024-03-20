bed2e8a0cf7938b366d9dac8755d4dded08e13f4
Alioth <3434039767@qq.com>
2024-03-20 22:40:38 +0800

    3_20_22_40

--- Experiment_1/T_1.cpp
+++ Experiment_1/T_1.cpp
@@ -20,7 +20,6 @@ void CreateSeqList(SeqList &L)
         cin >> temp;
         for (int j = i; j > -1; j--)
         {
-
             if (temp < L.data[j])
             {
                 L.data[j + 1] = L.data[j];
@@ -35,19 +34,77 @@ void CreateSeqList(SeqList &L)
         L.length++;
     }
 }
+
 void OutputSeqList(SeqList &L)
 {
-
     for (int i = 0; i < L.length; i++)
         cout << L.data[i] << " ";
     cout << endl;
 }
 
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+    int length = 0;
+} LinkList;
+
+void CreateLinkList(LinkList *&head)
+{
+    LinkList *q = new LinkList;
+    q->next = NULL;
+    head->next = q; // 将头指针指向第一个节点
+    cin >> q->data;
+    LinkList *r = head->next;
+    for (int i = 0; i < MaxSize - 1; i++)
+    {
+        LinkList *p = new LinkList;
+        p->next = NULL;
+        cin >> p->data;
+        r = head;
+        while (!r)
+        {
+            if (p->data < r->data)
+            {
+                p->next = r->next;
+                r->next = p;
+            }
+            else
+            {
+                r = r->next;
+            }
+        }
+
+        p = NULL;
+
+        head->length++; // 更新头指针的
+    }
+}
+
+void OutputLinkList(LinkList &L, LinkList *head)
+{
+    LinkList *p = head;
+    while (p != NULL)
+    {
+        cout << p->next->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+// 随机输入一组数，创建一个元素递增有序的单链表
 int main()
 {
-    SeqList L;
-    CreateSeqList(L);
-    OutputSeqList(L);
+    //SeqList L;
+    // CreateSeqList(L);
+    // OutputSeqList(L);
+    LinkList L2;
+    LinkList *head = new LinkList;
+    head->next = NULL;
+    head->length = 0;
+    head->data = 0;
+    CreateLinkList(head);
+    OutputLinkList(L2, head);
     system("pause");
     return 0;
 }
