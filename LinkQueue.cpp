e94b0768c8f725d2f4c2fd173dfe835fdf9a68cc
Alioth <3434039767@qq.com>
2024-04-04 11:01:43 +0800

    🎉 init: 链队列

---
+++ LinkQueue.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+class LinkQueue
+{
+private:
+    Node *front, *rear;
+
+public:
+    LinkQueue()
+    {
+        front = rear = NULL;
+    }
+
+    bool isEmpty()
+    {
+        return (front == rear) ? true : false;
+    }
+
+    void EnQueue(int val)
+    {
+        Node *s = new Node;
+        s->data = val;
+        s->next = NULL;
+
+        if (rear == NULL)
+        {
+            front = rear = s;
+            return;
+        }
+
+        rear->next = s;
+        rear = s;
+    }
+
+    int DeQueue()
+    {
+        if (isEmpty())
+        {
+            cout << "Queue is empty" << endl;
+            return -1;
+        }
+        
+        int val = front->data;
+        Node *p = front;
+        front = front->next;
+        delete p;
+        return val;
+        
+    }
+
+    void Output()
+    {
+        Node *p = front;
+        while (p!= NULL)
+        {
+            cout << p->data << " ";
+            p = p->next;
+        }
+        cout << endl;
+    }
+
+    int getSize()
+    {
+        int size = 0;
+        Node *p = front;
+        while (p!= NULL)
+        {
+            size++;
+            p = p->next;
+        }
+        return size;
+    }
+
+    ~LinkQueue()
+    {
+        Node *p = front;
+        while (p!= NULL)
+        {
+            Node *q = p;
+            p = p->next;
+            delete q;
+        }
+    }
+   
+   void Menu()
+    {
+        int choice;
+        do
+        {
+            cout << "1. Enqueue" << endl;
+            cout << "2. Dequeue" << endl;
+            cout << "3. Output" << endl;
+            cout << "4. Size" << endl;
+            cout << "5. Exit" << endl;
+            cout << "Enter your choice: ";
+            cin >> choice;
+
+            switch (choice)
+            {
+            case 1:
+            {
+                int val;
+                cout << "Enter value to enqueue: ";
+                cin >> val;
+                EnQueue(val);
+                break;
+            }
+            case 2:
+            {
+                int val = DeQueue();
+                if (val!= -1)
+                {
+                    cout << "Dequeued value: " << val << endl;
+                }
+                break;
+            }
+            case 3:
+            {
+                Output();
+                break;
+            }
+            case 4:
+            {
+                cout << "Size of queue: " << getSize() << endl;
+                break;
+            }
+            case 5:
+            {
+                cout << "Exiting..." << endl;
+                break;
+            }
+            default:
+            {
+                cout << "Invalid choice" << endl;
+                break;
+            }
+            }
+        } while (choice!= 5);
+    }
+    
+};
+
+int main()
+{
+    LinkQueue q;
+    q.Menu();
+    return 0;
+}
