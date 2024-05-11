d9b4af82dd995d148a6ca8a99036267c4a0f4462
Alioth <3434039767@qq.com>
2024-05-11 09:06:41 +0800

    🎉 init: 添加了一个冒泡排序实现，优化了排序算法，减少了不必要的比较。

---
+++ Sort/BubbleWithFlag.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void bubbleSort(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size() - 1; i++)
+    {
+        bool flag = false;
+        for (int j = 0; j < arr.size() - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                flag = true;
+            }
+        }
+        if (!flag)
+            break;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {5, 3, 8, 4, 2, 7, 1, 6};
+    bubbleSort(arr);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
