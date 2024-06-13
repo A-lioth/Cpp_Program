e60d34d2e501ad9d35c216a20e9b0136e906ae4f
Alioth <3434039767@qq.com>
2024-06-13 16:41:02 +0800

    🎉 init: 添加“可以种植最多的花朵”代码文件。

---
+++ LeetCode/贪心/T605_Can-Place-Flowers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool canPlaceFlowers(vector<int> &flowerbed, int n)
+{
+}
+
+int main()
+{
+    vector<int> flowerbed = {1, 0, 0, 0, 1};
+    int n = 1;
+    cout << canPlaceFlowers(flowerbed, n) << endl;
+    return 0;
+}
