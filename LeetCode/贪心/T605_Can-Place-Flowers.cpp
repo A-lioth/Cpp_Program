4a4f478bc012cecc69e8dca5c83bc3d0ef21e580
Alioth <3434039767@qq.com>
2024-06-14 11:10:06 +0800

    ✨ feat: 通过判断边界和中间的前后位置是否满足种植条件，满足则种植。最后判断是否全部种植完成。

--- LeetCode/贪心/T605_Can-Place-Flowers.cpp
+++ LeetCode/贪心/T605_Can-Place-Flowers.cpp
@@ -5,11 +5,22 @@ using namespace std;
 
 bool canPlaceFlowers(vector<int> &flowerbed, int n)
 {
+    for (int i = 0; i < flowerbed.size(); i++)
+    {
+        if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0))
+        {
+            flowerbed[i] = 1;
+            n--;
+        }
+        if (n <= 0)
+            return true;
+    }
+    return false;
 }
 
 int main()
 {
-    vector<int> flowerbed = {1, 0, 0, 0, 1};
+    vector<int> flowerbed = {0, 0, 1, 0, 1};
     int n = 1;
     cout << canPlaceFlowers(flowerbed, n) << endl;
     return 0;
