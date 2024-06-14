1e7d817a0f261c242af0ae6aee762fe67f534989
Alioth <3434039767@qq.com>
2024-06-14 11:16:25 +0800

    🎉 init: 添加“用最少数量的箭引爆气球”代码文件

---
+++ LeetCode/贪心/T452_Minimum-Number-of-Arrows-to-Burst-Balloons.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findMinArrowShots(vector<vector<int>> &points)
+{
+}
+
+int main()
+{
+    vector<vector<int>> points = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
+    cout << findMinArrowShots(points) << endl;
+    return 0;
+}
