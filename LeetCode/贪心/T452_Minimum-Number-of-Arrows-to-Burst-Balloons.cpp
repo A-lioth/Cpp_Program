75a7ce82ae8bd6740fc68c2b9c5389847790cfa2
Alioth <3434039767@qq.com>
2024-06-15 14:42:21 +0800

    ✨ feat: 使用与T435相似的解法实现了用最少数量的箭引爆气球。

--- LeetCode/贪心/T452_Minimum-Number-of-Arrows-to-Burst-Balloons.cpp
+++ LeetCode/贪心/T452_Minimum-Number-of-Arrows-to-Burst-Balloons.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int findMinArrowShots(vector<vector<int>> &points)
 {
+    if (points.empty())
+        return 0;
+    sort(points.begin(), points.end(), [](vector<int> &a, vector<int> &b)
+         { return a[1] < b[1]; });
+    int res = 0, end = points[0][1];
+    for (int i = 1; i < points.size(); i++)
+    {
+        if (points[i][0] <= end)
+            end = min(end, points[i][1]);
+        else
+        {
+            end = points[i][1];
+            res++;
+        }
+    }
+    return res + 1;
 }
 
 int main()
 {
-    vector<vector<int>> points = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
+    vector<vector<int>> points = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
     cout << findMinArrowShots(points) << endl;
     return 0;
 }
