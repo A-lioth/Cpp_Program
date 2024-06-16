cb863806f3a1b032505785b147d4b66df8caa0ec
Alioth <3434039767@qq.com>
2024-06-16 15:16:33 +0800

    🎉 init: 添加“买卖股票的最佳时机”代码文件。

---
+++ LeetCode/贪心/T122_Best-Time-to-Buy-and-Sell-Stock-ii.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxProfit(vector<int> &prices)
+{
+}
+
+int main()
+{
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << maxProfit(prices) << endl;
+    return 0;
+}
