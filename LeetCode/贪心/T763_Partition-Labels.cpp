f35644057c19180dfdbf2de9512c2f6d1ba29422
Alioth <3434039767@qq.com>
2024-06-16 15:11:08 +0800

    ✨ feat: 先将串中的每个字符最后出现的位置记录下来，通过判断下标和end指针的关系进行片段划分，最后将每个片段中的字符数量记录在result中返回。

--- LeetCode/贪心/T763_Partition-Labels.cpp
+++ LeetCode/贪心/T763_Partition-Labels.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> partitionLabels(string s)
 {
+    vector<int> res;
+    if (s.empty())
+    {
+        return res;
+    }
+
+    vector<int> last(26);
+    // 记录每个字符最后出现的位置
+    for (int i = 0; i < s.size(); i++)
+    {
+        last[s[i] - 'a'] = i;
+    }
+
+    int start = 0, end = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        end = max(end, last[s[i] - 'a']);
+        if (i == end)
+        {
+            res.push_back(end - start + 1);
+            start = end + 1;
+        }
+    }
+    return res;
 }
 
 int main()
