d9f6e092498b7faff8e76a426faedd2e24fd665a
Alioth <3434039767@qq.com>
2024-06-18 08:56:22 +0800

    🎉 init: 添加“非递减数列”代码文件。

---
+++ LeetCode/贪心/T665_Non-decreasing-Array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool checkPossibility(vector<int> &nums)
+{
+}
+
+int main()
+{
+    vector<int> nums = {4, 2, 3};
+    cout << checkPossibility(nums) << endl;
+    return 0;
+}
