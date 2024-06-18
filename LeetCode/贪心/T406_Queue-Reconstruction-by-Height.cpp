00c00db38b88623e54e30b2692d7339ac67cc29f
Alioth <3434039767@qq.com>
2024-06-18 08:53:32 +0800

    ✨ feat: 先按身高将people降序排列，相同身高按照比高人数升序排列，然后以比高人数为索引将不在正确位置上的元素插入其对应位置，最后返回重新构造的数组。

--- LeetCode/贪心/T406_Queue-Reconstruction-by-Height.cpp
+++ LeetCode/贪心/T406_Queue-Reconstruction-by-Height.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<vector<int>> reconstructQueue(vector<vector<int>> &people)
 {
+    sort(people.begin(), people.end(), [](vector<int> &a, vector<int> &b)
+         { return a[0] == b[0] ? a[1] > b[1] : a[0] > b[0]; });
+    vector<vector<int>> res;
+    for (auto p : people)
+        res.insert(res.begin() + p[1], p);
+    return res;
 }
 
 int main()
