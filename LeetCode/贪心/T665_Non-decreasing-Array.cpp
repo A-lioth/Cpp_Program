d56fac126733d3870977aa9a3f0122f2cb25cf3d
Alioth <3434039767@qq.com>
2024-06-19 20:13:26 +0800

    ✨ feat: 通过比较两个相邻元素的大小，判断是否需要修改元素值使得该序列成为非递减数列。如果是 i = 1 位置的元素或满足当前位置前面的第两个元素大于该元素，则将前一个元素的值修改为当前元素的值，否则将当前元素的值修改为前一个元素的值。最后，根据修改次数判断是否满足条件即可。

--- LeetCode/贪心/T665_Non-decreasing-Array.cpp
+++ LeetCode/贪心/T665_Non-decreasing-Array.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 bool checkPossibility(vector<int> &nums)
 {
+    int count = 0;
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i - 1] > nums[i])
+        {
+            count++;
+            if (count > 1)
+                return false;
+            if (i == 1 || nums[i - 2] <= nums[i])
+                nums[i - 1] = nums[i];
+            else
+                nums[i] = nums[i - 1];
+        }
+    }
+    return true;
 }
 
 int main()
