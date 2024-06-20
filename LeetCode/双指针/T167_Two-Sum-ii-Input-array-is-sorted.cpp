d0cf6f1514227bc11722136c97b6adfd3326cdd7
Alioth <3434039767@qq.com>
2024-06-20 08:29:54 +0800

    ✨ feat: 使用两个指针分别指向numbers的begin和end，比较begin和end的和和target的大小，如果小于则begin指向其下一个元素，否则end指向其前一个元素，直到begin和end的和与target相等或遍历结束为止。最后返回begin和end的索引。

--- LeetCode/双指针/T167_Two-Sum-ii-Input-array-is-sorted.cpp
+++ LeetCode/双指针/T167_Two-Sum-ii-Input-array-is-sorted.cpp
@@ -5,6 +5,18 @@ using namespace std;
 
 vector<int> twoSum(vector<int> &numbers, int target)
 {
+    int i = 0;
+    int j = numbers.size() - 1;
+    while (i < j)
+    {
+        if (numbers[i] + numbers[j] < target)
+            i++;
+        else if (numbers[i] + numbers[j] > target)
+            j--;
+        else
+            return vector<int>{i + 1, j + 1};
+    }
+    return vector<int>{-1, -1};
 }
 
 int main()
