da340aafda94d29d6945ca6d75aead2b42487237
Alioth <3434039767@qq.com>
2024-06-20 08:33:48 +0800

    🎉 init: 添加“合并两个有序数组”代码文件。

---
+++ LeetCode/双指针/T88_Merge-Sorted-Array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+{
+}
+
+int main()
+{
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    int m = 3;
+    vector<int> nums2 = {2, 5, 6};
+    int n = 3;
+    merge(nums1, m, nums2, n);
+    for (int i = 0; i < nums1.size(); i++)
+    {
+        cout << nums1[i] << " ";
+    }
+    return 0;
+}
