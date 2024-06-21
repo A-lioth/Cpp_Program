1272b403cdcf952813fa31a53b4e98bfb136f229
Alioth <3434039767@qq.com>
2024-06-21 10:56:57 +0800

    ✨ feat: 通过维护两个指在nums1和nums2实际末尾的指针first_end和second_end以及指在nums1和nums2合并成的nums1末尾的指针last_end，实现合并两个有序数组。当second_end大于first_end时，将second_end添加到last_end，并将second_end和last_end减1；当second_end小于等于first_end时，将first_end添加到last_end，并将first_end和last_end减1。最后，直到second_end小于0，则说明nums2已经全部添加到nums1中。

--- LeetCode/双指针/T88_Merge-Sorted-Array.cpp
+++ LeetCode/双指针/T88_Merge-Sorted-Array.cpp
@@ -5,14 +5,24 @@ using namespace std;
 
 void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
 {
+    int first_end = m - 1, second_end = n - 1;
+    int last_end = m + n - 1;
+
+    // nums1和nums2中均有元素
+    while (first_end >= 0 && second_end >= 0)
+        nums1[last_end--] = nums2[second_end] > nums1[first_end] ? nums2[second_end--] : nums1[first_end--];
+
+    // nums2中有剩余元素
+    while (second_end >= 0)
+        nums1[last_end--] = nums2[second_end--];
 }
 
 int main()
 {
-    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    vector<int> nums1 = {0, 0, 3, 0, 0, 0, 0, 0, 0};
     int m = 3;
-    vector<int> nums2 = {2, 5, 6};
-    int n = 3;
+    vector<int> nums2 = {-1, 1, 1, 1, 2, 3};
+    int n = 6;
     merge(nums1, m, nums2, n);
     for (int i = 0; i < nums1.size(); i++)
     {
