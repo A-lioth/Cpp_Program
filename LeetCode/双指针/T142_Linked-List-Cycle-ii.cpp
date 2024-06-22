732d36e02462257de6370b53bbbc83a1e82323d5
Alioth <3434039767@qq.com>
2024-06-22 09:31:08 +0800

    通过快慢指针的相遇判断链表是否存在环路。首先将两个指针都指向头结点，然后以fast为2节点/次，slow为1节点/次的速度移动指针。当两个指针第一次相遇时，说明链表存在环路。否则，说明链表不存在环路。如果存在环路，则fast重新指向头结点，然后和slow以相同的速度移动指针，直到两个指针再次相遇。相遇的节点即为环路的入口。最后，返回相遇的节点即可。

--- LeetCode/双指针/T142_Linked-List-Cycle-ii.cpp
+++ LeetCode/双指针/T142_Linked-List-Cycle-ii.cpp
@@ -7,14 +7,44 @@ struct ListNode
 {
     int val;
     ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(nullptr) {}
 };
 
 ListNode *detectCycle(ListNode *head)
 {
+    ListNode *slow = head, *fast = head;
+    // fast和slow第一次相遇，说明有环路
+    do
+    {
+        if (fast == nullptr || fast->next == nullptr)
+            return nullptr;
+        fast = fast->next->next;
+        slow = slow->next;
+    } while (fast != slow);
+    // fast从头开始，slow和fast每次移动一步，直到fast和slow相遇，此时slow指向环路入口
+    fast = head;
+    while (fast != slow)
+    {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    return fast;
 }
 
 int main()
 {
+    ListNode *head = new ListNode(3);
+    ListNode *second = new ListNode(2);
+    ListNode *third = new ListNode(0);
+    ListNode *fourth = new ListNode(-4);
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = second;
+    ListNode *node = detectCycle(head);
+    if (node)
+        cout << "Cycle detected at node " << node->val << endl;
+    else
+        cout << "No cycle detected" << endl;
     return 0;
 }
