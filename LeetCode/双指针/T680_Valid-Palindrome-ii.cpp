739c9bc4a3d9a936a9a360bff7d9aca06aea00ff
Alioth <3434039767@qq.com>
2024-06-24 10:02:54 +0800

    🎉 init: 添加“验证回文串”代码文件。

---
+++ LeetCode/双指针/T680_Valid-Palindrome-ii.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool validPalindrome(string s)
+{
+}
+
+int main()
+{
+    string s = "abc";
+    cout << validPalindrome(s) << endl;
+    return 0;
+}
