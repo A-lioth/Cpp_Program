ed76ea6007a33d5e3a60bccdfedce6c9e29aa992
Alioth <3434039767@qq.com>
2024-06-24 09:59:35 +0800

    ✨ feat: 使用和T240_Search-a-2D-Matrix-ii 相同的思路完成。初始化sum为矩阵右上角的元素，并与 c 比较。若 sum  < c，则 left++，排除上一行元素；若 sum > c，则 right--，排除右一列元素。最后，sum = c，则返回 true，否则，返回 false。

--- LeetCode/双指针/T633_Sum-of-Square-Numbers.cpp
+++ LeetCode/双指针/T633_Sum-of-Square-Numbers.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-bool judgeSquareSum(int c) 
+bool judgeSquareSum(int c)
 {
+    long left = 0, right = sqrt(c);
+    while (left <= right)
+    {
+        long sum = left * left + right * right;
+        if (sum < c)
+            left++;
+        else if (sum > c)
+            right--;
+        else
+            return true;
+    }
+    return false;
 }
 
-int main() 
+int main()
 {
-    int c = 5;
+    int c = 1000000;
     cout << judgeSquareSum(c) << endl;
     return 0;
 }
