512ad6760f70d6c5344f81271f48b0604b212454
Alioth <3434039767@qq.com>
2024-06-25 10:20:00 +0800

    ✨ feat: 使用双指针判断原始字符串是否为回文串，不是则递归判断 left + 1， right 是否为回文串，不是继续递归 left，right - 1 是否为回文串，如此即可判断原始字符串是否为回文串。

--- LeetCode/双指针/T680_Valid-Palindrome-ii.cpp
+++ LeetCode/双指针/T680_Valid-Palindrome-ii.cpp
@@ -3,13 +3,25 @@
 
 using namespace std;
 
+bool isPalindrome(string s, int left, int right)
+{
+    while (left < right)
+        if (s[left++] != s[right--])
+            return false;
+    return true;
+}
+
 bool validPalindrome(string s)
 {
+    for (int left = 0, right = s.size() - 1; left < right; left++, right--)
+        if (s[left] != s[right])
+            return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+    return true;
 }
 
 int main()
 {
-    string s = "abc";
+    string s = "abcdeca";
     cout << validPalindrome(s) << endl;
     return 0;
 }
