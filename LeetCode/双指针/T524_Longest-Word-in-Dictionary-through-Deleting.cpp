c1cf245bcccad87ecc1a17ab262f4a91d2a016af
Alioth <3434039767@qq.com>
2024-06-26 10:01:09 +0800

    ✨ feat: 首先按照以下规则对 dictionary 进行排序：长度不同，按照长度排倒序；长度相同，则按照字典序排升序。然后使用两个指针 i 和 j 依次完成 dictionary 中的字符串在 s 中的匹配。最后，返回匹配的结果。

--- LeetCode/双指针/T524_Longest-Word-in-Dictionary-through-Deleting.cpp
+++ LeetCode/双指针/T524_Longest-Word-in-Dictionary-through-Deleting.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 string findLongestWord(string s, vector<string> &dictionary)
 {
+    sort(dictionary.begin(), dictionary.end(), [](string &a, string &b)
+         {
+            if(a.length() == b.length()) return a < b;
+            return a.length() > b.length(); });
+
+    for (string ds : dictionary)
+    {
+        if (ds.length() > s.length())
+            continue;
+        int i = 0, j = 0;
+        while (i < s.length() && j < ds.length())
+        {
+            if (s[i] == ds[j])
+                j++;
+            i++;
+            if (j == ds.length())
+                return ds;
+        }
+    }
+    return "";
 }
 
 int main()
