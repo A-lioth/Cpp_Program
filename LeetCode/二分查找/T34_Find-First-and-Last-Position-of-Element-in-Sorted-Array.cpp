7d04936e8cce1acd5b4e0885c8d26244c46dc275
Alioth <3434039767@qq.com>
2024-06-28 09:40:08 +0800

    ✨ feat: 通过使用两次二分查找实现在排序数组中查找元素出现的第一个和最后一个位置。

--- LeetCode/二分查找/T34_Find-First-and-Last-Position-of-Element-in-Sorted-Array.cpp
+++ LeetCode/二分查找/T34_Find-First-and-Last-Position-of-Element-in-Sorted-Array.cpp
@@ -3,8 +3,43 @@
 
 using namespace std;
 
+int lower_bound(vector<int> &nums, int target)
+{
+    int left = 0, right = nums.size() - 1;
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+        if (target > nums[mid])
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return left;
+}
+
+int upper_bound(vector<int> &nums, int target)
+{
+    int left = 0, right = nums.size() - 1;
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+        if (target > nums[mid])
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return left;
+}
+
 vector<int> searchRange(vector<int> &nums, int target)
 {
+    if (nums.empty())
+        return {-1, -1};
+    int left = lower_bound(nums, target);
+    if (left == nums.size() || nums[left] != target)
+        return {-1, -1};
+    int right = upper_bound(nums, target + 1) - 1;
+    return {left, right};
 }
 
 int main()
