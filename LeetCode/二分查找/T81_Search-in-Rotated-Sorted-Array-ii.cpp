bcadcd3c6395a704c9262f27b642b9133f3aafe8
Alioth <3434039767@qq.com>
2024-06-29 08:49:46 +0800

    ✨ feat: 首先检查 mid 是否为目标值，是则直接返回 true。然后检查左区间是否有序，判断目标值是否在左区间，是则调整 right。检查右区间是否有序，然后判断目标值是否在右区间，是则调整 left。如果 left 与 mid 相等，则移动 left 防止重复元素导致的死循环。最后，若循环结束仍未找到目标值，则返回 false。

--- LeetCode/二分查找/T81_Search-in-Rotated-Sorted-Array-ii.cpp
+++ LeetCode/二分查找/T81_Search-in-Rotated-Sorted-Array-ii.cpp
@@ -5,12 +5,36 @@ using namespace std;
 
 bool search(vector<int> &nums, int target)
 {
+    int left = 0, right = nums.size() - 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target)
+            return true;
+        if (nums[left] < nums[mid])
+        {
+            if (target >= nums[left] && target < nums[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        else if (nums[left] > nums[mid])
+        {
+            if (target > nums[mid] && target <= nums[right])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        else
+            left++;
+    }
+    return false;
 }
 
 int main()
 {
-    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
-    int target = 0;
+    vector<int> nums = {1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1};
+    int target = 2;
     bool result = search(nums, target);
     if (result)
         cout << "Target " << target << " found in the array." << endl;
