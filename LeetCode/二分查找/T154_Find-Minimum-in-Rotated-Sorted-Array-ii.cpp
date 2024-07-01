2c3ba82e81ca3b167cb4500ab109cbf3c529d218
Alioth <3434039767@qq.com>
2024-07-01 09:25:03 +0800

    ✨ feat: 使用二分查找实现寻找旋转排序数组中的最小值。当 mid > right 时，m 一定在左区间中，即旋转点 x 一定在 [mid + 1, right] 内，执行 left = mid + 1。当 mid < right 时，m 一定在右区间中，即旋转点 x 一定在[left, mid] 内，执行 right = mid。当mid = right 时，无法判断 mid 在哪个区间中，即无法判断旋转点 x 在 [left, mid] 还是 [mid+1, right] 中。执行 right--  缩小判断范围。最后，返回 left 所指元素即为旋转排序数组中的最小值。

--- LeetCode/二分查找/T154_Find-Minimum-in-Rotated-Sorted-Array-ii.cpp
+++ LeetCode/二分查找/T154_Find-Minimum-in-Rotated-Sorted-Array-ii.cpp
@@ -5,11 +5,23 @@ using namespace std;
 
 int findMin(vector<int> &nums)
 {
+    int left = 0, right = nums.size() - 1;
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] > nums[right])
+            left = mid + 1;
+        else if (nums[mid] < nums[right])
+            right = mid;
+        else
+            right--;
+    }
+    return nums[left];
 }
 
 int main()
 {
-    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    vector<int> nums = {3, 3, 1, 3};
     cout << findMin(nums) << endl;
     return 0;
 }
