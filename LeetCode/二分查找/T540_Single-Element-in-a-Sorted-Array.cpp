6282758043d17e27da5f28a4e6b8ded5772404ea
Alioth <3434039767@qq.com>
2024-07-01 09:27:18 +0800

    🎉 init: 添加“有序数组中的单一元素”代码文件。

---
+++ LeetCode/二分查找/T540_Single-Element-in-a-Sorted-Array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int singleNonDuplicate(vector<int> &nums)
+{
+}
+
+int main()
+{
+    vector<int> nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+    cout << singleNonDuplicate(nums) << endl;
+    return 0;
+}
