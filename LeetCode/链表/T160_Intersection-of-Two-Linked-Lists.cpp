a7ae91698d80f773a2276d05f67103f6868f6c92
Alioth <3434039767@qq.com>
2024-07-05 22:15:44 +0800

    ✨ feat: 使用双指针判断给定的两个链表是否交于一点，并求相交的节点。首先使用 pA 指向链表 A 的头节点，pB 指向链表 B 的头节点。然后同时遍历两个链表，直到 pA 到达链表 A 的尾节点或 pB 到达链表 B 的尾节点。如果 pA 到达链表 A 的尾节点，则 pA 开始指向链表 B 的头节点继续遍历。如果 pB 到达链表 B 的尾节点，则 pB 开始指向链表 A 的头节点继续遍历。最后，如果 pA 和 pB 相等，则说明链表 A 和链表 B 相交于 pA 节点，返回 pA 节点。

--- LeetCode/链表/T160_Intersection-of-Two-Linked-Lists.cpp
+++ LeetCode/链表/T160_Intersection-of-Two-Linked-Lists.cpp
@@ -12,6 +12,15 @@ struct ListNode
 
 ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
 {
+    if (headA == nullptr || headB == nullptr)
+        return nullptr;
+    ListNode *pA = headA, *pB = headB;
+    while (pA != pB)
+    {
+        pA = pA == nullptr ? headB : pA->next;
+        pB = pB == nullptr ? headA : pB->next;
+    }
+    return pA;
 }
 
 int main()
