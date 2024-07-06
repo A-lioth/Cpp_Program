a0200699e745ca4b2cca9e83f4b30b55c6922fac
Alioth <3434039767@qq.com>
2024-07-06 11:33:41 +0800

    ✨ feat: 使用双指针和反转链表实现判断链表是否回文。首先，使用快慢指针找到链表的中点，然后使用 reverseList() 将后半段链表反转，并将反转后链表的头部和反转前链表的中点连接。最后，同时遍历比较前半段和反转后的后半段链表的元素值，判断链表是否回文并返回比较的结果。

--- LeetCode/链表/T234_Palindrome-Linked-List.cpp
+++ LeetCode/链表/T234_Palindrome-Linked-List.cpp
@@ -11,8 +11,44 @@ struct ListNode
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+ListNode *reverseList(ListNode *head)
+{
+    ListNode *prev = nullptr;
+    while (head != nullptr)
+    {
+        ListNode *temp = head->next;
+        head->next = prev;
+        prev = head;
+        head = temp;
+    }
+    return prev;
+}
+
 bool isPalindrome(ListNode *head)
 {
+    if (head == nullptr || head->next == nullptr)
+        return true;
+    // 找到链表的中间节点
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast->next != nullptr && fast->next->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    // 翻转后半部分链表
+    slow->next = reverseList(slow->next);
+    // 找到前半部分链表的尾节点
+    slow = slow->next;
+    // 逐个比较前半部分链表和翻转后的后半部分链表
+    while (slow != nullptr)
+    {
+        if (slow->val != head->val)
+            return false;
+        slow = slow->next;
+        head = head->next;
+    }
+    return true;
 }
 
 int main()
