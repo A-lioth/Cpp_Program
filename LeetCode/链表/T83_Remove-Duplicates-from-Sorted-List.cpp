86542c6055ea33f8faf5b2ea2c2dc271db6dff67
Alioth <3434039767@qq.com>
2024-07-07 22:02:53 +0800

    ✨ feat: 使用双指针实现删除非递减链表中的重复元素。

--- LeetCode/链表/T83_Remove-Duplicates-from-Sorted-List.cpp
+++ LeetCode/链表/T83_Remove-Duplicates-from-Sorted-List.cpp
@@ -13,10 +13,38 @@ struct ListNode
 
 ListNode *deleteDuplicates(ListNode *head)
 {
+    if (head == nullptr)
+        return nullptr;
+
+    ListNode *prev = head;
+    ListNode *curr = head->next;
+
+    while (curr != nullptr)
+    {
+        if (curr->val == prev->val)
+        {
+            prev->next = curr->next;
+            curr = prev->next;
+        }
+        else
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+
+    return head;
 }
 
 int main()
 {
     ListNode *head = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))));
+    head = deleteDuplicates(head);
+    while (head != nullptr)
+    {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
     return 0;
 }
