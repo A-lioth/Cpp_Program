c4aa2a63df9ec97b4f54d463c7ca95f5fec1af0d
Alioth <3434039767@qq.com>
2024-07-08 21:09:59 +0800

    ✨ feat: 使用两个指针交叉遍历连接实现将所有索引为奇数的节点和索引为偶数的节点分别组合在一起，然后返回重新排序的链表。

--- LeetCode/链表/T328_Odd-Even-Linked-List.cpp
+++ LeetCode/链表/T328_Odd-Even-Linked-List.cpp
@@ -13,6 +13,21 @@ struct ListNode
 
 ListNode *oddEvenList(ListNode *head)
 {
+    if (head == nullptr || head->next == nullptr)
+        return head;
+
+    ListNode *odd = head;
+    ListNode *even = head->next;
+    ListNode *evenHead = even;
+    while (even != nullptr && even->next != nullptr)
+    {
+        odd->next = even->next;
+        odd = odd->next;
+        even->next = odd->next;
+        even = even->next;
+    }
+    odd->next = evenHead;
+    return head;
 }
 
 int main()
