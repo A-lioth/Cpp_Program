43d526b0b9669632e75bda6c5c14dca6ed4cd9cf
Alioth <3434039767@qq.com>
2024-07-10 19:19:51 +0800

    ✨ feat: 聚合求链表中点、合并两个有序链表的算法，实现给一个链表排序。

--- LeetCode/链表/T148_Sort-List.cpp
+++ LeetCode/链表/T148_Sort-List.cpp
@@ -11,8 +11,55 @@ struct ListNode
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+ListNode *middleNode(ListNode *head)
+{
+    if (head == nullptr || head->next == nullptr)
+        return head;
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+{
+    ListNode dummy(0);
+    ListNode *tail = &dummy;
+    while (list1 && list2)
+    {
+        if (list1->val < list2->val)
+        {
+            tail->next = list1;
+            list1 = list1->next;
+        }
+        else
+        {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        tail = tail->next;
+    }
+    tail->next = list1? list1 : list2;
+    return dummy.next;
+}
+
 ListNode *sortList(ListNode *head)
 {
+    if (head == nullptr || head->next == nullptr)
+        return head;
+    // 找到链表的中间节点并分割成两个链表
+    ListNode *mid = middleNode(head);
+    ListNode *rightHead = mid->next;
+    mid->next = nullptr;
+    // 递归排序两个链表
+    ListNode *left = sortList(head);
+    ListNode *right = sortList(rightHead);
+    // 合并两个链表
+    return mergeTwoLists(left, right);
 }
 
 int main()
