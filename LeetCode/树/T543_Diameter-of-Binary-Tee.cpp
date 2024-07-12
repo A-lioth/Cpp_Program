c77fefe9cf65b9a71b1668abca493192f1702909
Alioth <3434039767@qq.com>
2024-07-12 11:15:57 +0800

    🎉 init: 添加“二叉树的直径”代码文件。

---
+++ LeetCode/树/T543_Diameter-of-Binary-Tee.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int diameterOfBinaryTree(TreeNode *root)
+{
+}
+
+int main()
+{
+    TreeNode *root = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3, nullptr, new TreeNode(6)));
+    cout << diameterOfBinaryTree(root) << endl;
+    return 0;
+}
