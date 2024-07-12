b0129aae98ea7011c549199878cedb47d85b9de1
Alioth <3434039767@qq.com>
2024-07-12 11:12:23 +0800

    ✨ feat: 使用先序遍历和求二叉树深度判断一个二叉树是否平衡。

--- LeetCode/树/T110_Balanced-Binary-Tree.cpp
+++ LeetCode/树/T110_Balanced-Binary-Tree.cpp
@@ -8,17 +8,23 @@ struct TreeNode
     TreeNode *left;
     TreeNode *right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(NULL), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+int height(TreeNode *root)
+{
+    return root == nullptr ? 0 : max(height(root->left), height(root->right)) + 1;
+}
+
 bool isBalanced(TreeNode *root)
 {
+    return root == nullptr ? true : isBalanced(root->left) && isBalanced(root->right) && abs(height(root->left) - height(root->right)) <= 1;
 }
 
 int main()
 {
-    TreeNode *root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+    TreeNode *root = new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3)));
     if (isBalanced(root))
         cout << "Balanced";
     else
