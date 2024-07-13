bcb9a5180d0f39931c2573b5cd8dc99d03ce121c
Alioth <3434039767@qq.com>
2024-07-13 09:27:42 +0800

    ✨ feat: 使用递归求一个二叉树的最长直径。每次先分别确定左右子树的高度，然后通过比较更新当前的最长直径，最后比较左右子树的最长直径，取最大值并加一得出二叉树的最长直径。

--- LeetCode/树/T543_Diameter-of-Binary-Tee.cpp
+++ LeetCode/树/T543_Diameter-of-Binary-Tee.cpp
@@ -12,8 +12,21 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+int height(TreeNode *root, int &prev)
+{
+    if (root == nullptr)
+        return 0;
+    int left = height(root->left, prev);
+    int right = height(root->right, prev);
+    prev = max(prev, left + right);
+    return 1 + max(left, right);
+}
+
 int diameterOfBinaryTree(TreeNode *root)
 {
+    int prev = 0;
+    height(root, prev);
+    return prev;
 }
 
 int main()
