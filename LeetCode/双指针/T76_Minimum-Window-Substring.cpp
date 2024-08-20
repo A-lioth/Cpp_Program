6be0f8f2655499fe7985e8e8a15d2db3227df65a
Alioth <3434039767@qq.com>
2024-08-20 17:05:43 +0800

    ✨ feat: 使用 right 指针遍历 s。每次移动 right 时，更新 count 并检查是否匹配到一个字符。当 cnt = 0 时，表示当前窗口包含所有 t 中的字符，尝试更新最小窗口。移动 left，缩小窗口，直到再包含所有 t 中的字符，期间更新最小窗口的长度和起始位置。最后，比较最小窗口的长度和 s 的长度，返回空字符串或最小窗口的子字符串。

--- LeetCode/双指针/T76_Minimum-Window-Substring.cpp
+++ LeetCode/双指针/T76_Minimum-Window-Substring.cpp
@@ -1,43 +1,34 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 string minWindow(string s, string t)
 {
-    vector<int> chars(128, 0);
-    vector<bool> flag(128, false);
+    unordered_map<char, int> count;
     for (char c : t)
-    {
-        flag[c] = true;
-        chars[c]++;
-    }
+        count[c]++;
 
-    int left = 0, right = 0, count = 0;
-    int min_len = s.size() + 1, min_left = 0, min_right = 0;
-
+    int left = 0, right = 0, cnt = t.size();
+    int min_len = s.size() + 1, min_left = 0;
     for (right = 0; right < s.size(); right++)
     {
-        if (flag[s[right]])
-        {
-            if (--chars[s[right]] >= 0)
-                count++;
-        }
-        while (count == t.size())
+        if (--count[s[right]] >= 0)
+            cnt--;
+        while (cnt == 0)
         {
             if (right - left + 1 < min_len)
             {
                 min_len = right - left + 1;
                 min_left = left;
-                min_right = right;
             }
-            if (flag[s[left]] && ++chars[s[left]] > 0)
-                count--;
+            if (++count[s[left]] > 0)
+                cnt++;
             left++;
         }
     }
-
+    
     return min_len > s.size() ? "" : s.substr(min_left, min_len);
 }
 
