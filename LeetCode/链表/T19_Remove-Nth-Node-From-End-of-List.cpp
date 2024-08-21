3c6e2a23c5d2bc94d9eff5087e1ef58a3695a838
Alioth <3434039767@qq.com>
2024-08-21 17:59:47 +0800

    ✨ feat: 更新 ListNode 构造函数实现链式初始化 head。

--- LeetCode/链表/T19_Remove-Nth-Node-From-End-of-List.cpp
+++ LeetCode/链表/T19_Remove-Nth-Node-From-End-of-List.cpp
@@ -7,7 +7,7 @@ struct ListNode
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(NULL) {}
+    ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
